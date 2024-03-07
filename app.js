429c6c0fb2123bccb7dee7709bed6fe1ef84cde6
MAVIAIBRAHIM768 <162436305+MAVIAIBRAHIM768@users.noreply.github.com>
2024-03-07 08:36:18 -0800

    ( 45 questions)  comprttion
    ( 45 questions) my second assigements work(Name Cases )

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+// 1st,lowercase ,2nd,uppercase and 3rd,titlecase,
+// 1st, lower case
+let personName = "ameer mavia";
+console.log("lowercase:", personName.toLowerCase());
+// 2nd upper case
+console.log("UPPERCASE:", personName.toLocaleUpperCase());
+// 3RD TITLE CASE.
+console.log("titlecse:", personName.
+    replace(/\bw/g, c => c.toUpperCase()));
